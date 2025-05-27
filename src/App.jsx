60313e66dd36894ace08ee43e9003052e84effd7
Ruhul Amin <ruhul.amin.sde@gmail.com>
2025-05-27 10:58:17 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Test from './test'; // Component name starts with uppercase
+
+const App = () => {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+};
+
+export default App;
